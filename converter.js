79e409b185baf809e62f700c8b38d688797941de
Elisei <strashevsky.elisey@gmail.com>
2022-02-08 13:38:58 +0300

    date-fns

--- converter.js
+++ converter.js
@@ -1,18 +1,13 @@
-export function convertTime(timestamp) {
-  const date = new Date(timestamp * 1000);
-  const hours = date.getHours();
-  const minutes = "0" + date.getMinutes();
-  const formattedTime = hours + ":" + minutes.slice(-2);
+import { format, fromUnixTime } from "date-fns";
 
+export function convertTime(timestamp) {
+  const fromUnix = fromUnixTime(timestamp);
+  const formattedTime = format(fromUnix, "p");
   return formattedTime;
 }
 
 export function convertDate(timestamp) {
-  const date = new Date(timestamp * 1000);
-  const day = date.toLocaleString("en-US", { day: "numeric" });
-  const month = date.toLocaleString("en-US", { month: "short" });
-
-  const formattedDate = `${day} ${month}`;
-
+  const fromUnix = fromUnixTime(timestamp);
+  const formattedDate = format(fromUnix, "d LLL");
   return formattedDate;
 }
